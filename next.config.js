8ecc8d16c9d16484e4553fb625eab172cbc33cd9
Abdfn <64256993+abdfnx@users.noreply.github.com>
2024-05-31 11:50:23 +0000

    start building 🦙

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  cacheOnFrontendNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: false,
+});
+
+module.exports = withPWA({});
