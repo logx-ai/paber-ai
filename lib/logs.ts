6691cb08a3dbf4623383454214a49ca3903a74b4
Abdfn <64256993+abdfnx@users.noreply.github.com>
2024-06-06 12:03:34 +0000

    research-ai(db): start initializing supabase 📦

---
+++ lib/logs.ts
@@ -0,0 +1,25 @@
+import { LogSnag } from "@logsnag/node";
+
+export const logsnag = new LogSnag({
+  token: process.env.NEXT_PUBLIC_LOGSNAG!,
+  project: "logx",
+});
+
+export const sendError = async (feature: any, desc: any) => {
+  await logsnag.track({
+    channel: "bugs",
+    event: `New Error: "${feature} Error"`,
+    icon: "🐞",
+    description: desc,
+    notify: true,
+    tags: {
+      feature,
+    },
+  });
+
+  await logsnag.insight.increment({
+    title: "Bugs, Errors & Craches",
+    value: 1,
+    icon: "🐞",
+  });
+};
